73c7b1d9698a67264d7a9223d997b165a06c87cf
alexc <alexcolwell13@gmail.com>
2023-06-04 13:39:43 -0500

    final

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,64 @@
 // Assignment code here
-var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*'];
-var numberCharacters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-var generateBtn = document.querySelector("#generate");
 
 
+
+function generatePassword() {
+  var lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  var uppercaseLetters = 'ABCDEFGHIJKELMNOPQRSTUVWXYZ';
+  var specialCharacters = "!#$%&()*+";
+  var numberCharacters = '1234567890';
+  var passwordArray = ""
+  var finalPassword = ""
+  var passwordLength = ""
+
+  while (isNaN(parseInt(passwordLength)) || parseInt(passwordLength) < 8 || parseInt(passwordLength) > 128) {
+    passwordLength = prompt("Password length needs to be between 8-128 characters")
+
+  }
+
+  var lowerCase = confirm("Would you like to include lowercase letters?");
+  var upperCase = confirm("Would you like to include uppercase letters?");
+  var specCharacters = confirm("Would you like to include special characters?");
+  var numbers = confirm("Would you like to include numbers?");
+  if (lowerCase) {
+    passwordArray += lowercaseLetters;
+    
+  }
+  if (upperCase) {
+    passwordArray += uppercaseLetters;
+    
+  }
+  if (specCharacters) {
+    passwordArray += specialCharacters;
+    
+  }
+  if (numbers) {
+    passwordArray += numberCharacters;
+    
+  }
+  if (!lowerCase && !upperCase && !specCharacters && !numbers)
+    alert("You must choose at least one option")
+
+
+
+
+  //for loop to iterate through the password array that has been concat with selected characters
+  for (var i = 0; i < passwordLength; i++) {
+    
+    var index = Math.floor(Math.random() * passwordArray.length);
+    
+    
+    finalPassword += passwordArray[index];
+    
+  }
+  console.log(finalPassword);
+  return finalPassword;
+
+  
+
+}
 // Get references to the #generate element
-
+var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -19,3 +70,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
